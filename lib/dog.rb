3fe5988901c30813e1c66b17727dd09223524029
Andres Manco <andresmanco@gmail.com>
2018-06-28 04:41:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,16 @@
+<<<<<<< HEAD
 require 'pry'
+=======
+>>>>>>> b922c37c6e73e65221bf4b73f89b15acc4f250d3
 class Dog
   @@all = []
   def initialize(name)
     @name = name
+<<<<<<< HEAD
     @@all << self
+=======
+    @@all << name
+>>>>>>> b922c37c6e73e65221bf4b73f89b15acc4f250d3
   end
   
   attr_reader :name
@@ -12,10 +19,12 @@ class Dog
     @@all = []
   end
   
+<<<<<<< HEAD
   def self.all
     @@all.each do |dog|
       puts dog.name
     end
   end
+
 
 end
